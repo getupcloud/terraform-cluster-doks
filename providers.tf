7c5fa4f44d0c24e14158e4683b50c2d0abc6c04e
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2021-08-11 11:51:23 -0300

    DOKS: configure kubectl provider

--- providers.tf
+++ providers.tf
@@ -10,3 +10,11 @@ terraform {
 provider "digitalocean" {
   token = var.do_token
 }
+
+provider "kubectl" {
+  load_config_file       = false
+  host                   = local.api_endpoint
+  cluster_ca_certificate = local.certificate_authority_data
+  token                  = local.token
+  apply_retry_count      = 2
+}
