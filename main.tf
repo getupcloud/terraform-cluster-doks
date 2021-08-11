cdad600271158acb6488987bb74e1565dcb56269
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2021-08-11 11:43:31 -0300

    DOKS: Add flux

--- main.tf
+++ main.tf
@@ -44,3 +44,10 @@ resource "digitalocean_kubernetes_node_pool" "node_pool" {
     effect = try(var.node_pool.taint.effect, null)
   }
 }
+
+module "flux" {
+  source = "github.com/getupcloud/terraform-module-flux?ref=main"
+
+  git_repo       = var.flux_git_repo
+  manifests_path = "./clusters/${var.name}/doks/manifests"
+}
