7eecfcbd882f90f43481e98637fc2d125b11f15f
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2021-08-11 11:55:44 -0300

    fix locals

--- locals.tf
+++ locals.tf
@@ -1,9 +1,9 @@
 locals {
-  api_endpoint               = module.cluster.kube_config.0.host
-  certificate_authority_data = base64decode(module.cluster.kube_config.0.cluster_ca_certificate)
-  token                      = module.cluster.kube_config.0.token
-  client_key                 = module.cluster.kube_config.0.client_key
-  client_certificate         = module.cluster.kube_config.0.client_certificate
+  api_endpoint               = digitalocean_kubernetes_cluster.cluster.kube_config.0.host
+  certificate_authority_data = base64decode(digitalocean_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)
+  token                      = digitalocean_kubernetes_cluster.cluster.kube_config.0.token
+  client_key                 = digitalocean_kubernetes_cluster.cluster.kube_config.0.client_key
+  client_certificate         = digitalocean_kubernetes_cluster.cluster.kube_config.0.client_certificate
   suffix                     = random_string.suffix.result
   secret                     = random_string.secret.result
 }
